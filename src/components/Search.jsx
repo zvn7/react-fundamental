1ee261de5006c2b75a1db35fce9cbdc53b4d3cd4
Ilham Maulana <aammaul7@gmail.com>
2024-07-15 14:24:52 +0700

    onClick Methods

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
-import { Input, Typography } from "@material-tailwind/react";
+import { Button, Input, Typography } from "@material-tailwind/react";
 
 export default function Search(props) {
 	const [search, setSearch] = useState("");
-	const onSearchChange = (e) => {
-		setSearch(e.target.value);
-		props.onSearchChange(e.target.value);
+	const onSearchChange = () => {
+		props.onSearchChange(search);
+	};
+
+	const searchKeyDown = (e) => {
+		if (e.key === "Enter") {
+			onSearchChange();
+		}
 	};
 
 	return (
 		<div className="w-96 items-center">
-			<Input onChange={onSearchChange} label="Search" value={search} />
+			<div className="flex space-x-2">
+				<Input onChange={e => setSearch(e.target.value)} onKeyDown={searchKeyDown} label="Search" value={search} />
+				<Button onClick={onSearchChange}>Cari</Button>
+			</div>
 			{search && (
 				<Typography color="gray" variant="small">
 					Ditemukan {props.resultsCount} data dari pencarian "{search}"
